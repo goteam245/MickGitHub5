6247ada6f5f7abe9cbc9fd3974dc5762046b6d32
goteam245 <jimmymicklas@yahoo.com>
2015-01-25 22:34:18 -0500

    Update Teleop.java

--- MickGitHubTest5/src/org/usfirst/frc/team245/robot/Teleop.java
+++ MickGitHubTest5/src/org/usfirst/frc/team245/robot/Teleop.java
@@ -2,7 +2,7 @@ package org.usfirst.frc.team245.robot;
 
 public class Teleop {
 	public void update() {
-		System.out.println("SmartDash master update");
+		System.out.println("SmartDash master update master");
 	}
 
 }
